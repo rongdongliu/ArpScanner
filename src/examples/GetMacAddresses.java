3b7efaedd6e4de778ed070ca541635058c442ca8
victorKovalchuck <victorkovalchuck@gmail.com>
2016-01-19 15:23:21 +0200

    project files

--- src/examples/GetMacAddresses.java
+++ src/examples/GetMacAddresses.java
@@ -38,7 +38,7 @@ public class GetMacAddresses {
 	}
 	
 	private static void sendArpToAll(){
-		for(int i = 1; i < 10; i++){	
+		for(int i = 1; i < 256; i++){	
 			sendArpRequest("192.168.1."+Integer.toString(i));					
 		}
 	}
